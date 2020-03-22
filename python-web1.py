38bd19f68a29590466843c87631b5ececb854b4d
tbriche <tbriche@localhost.localdomain>
2020-03-22 23:47:09 +0100

    Version 1 - simple affichage d'une requete WEB

---
+++ python-web1.py
@@ -0,0 +1,15 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+print("NATAS 0")
+
+# Création des variables du script
+user = "natas0"
+password = "natas0"
+url = "http://natas0.natas.labs.overthewire.org/"
+
+# Envoi de la requête avec l'authentificaiton HTTPBasicAuth
+res = requests.get(url, auth=HTTPBasicAuth(user, password))
+
+# Affichage de la réponse
+print(res.text)
